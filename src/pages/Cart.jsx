93fc45d5c9258163a775672d84f90c663417fac8
Adarsh Verma <adarsh878856@gmail.com>
2023-10-19 20:39:17 +0530

    add cart components with multiple time used

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,7 @@
-import { useDispatch, useSelector } from "react-redux";
-import { NavLink } from "react-router-dom";
-import { increaseQty, decreaseQty, removeFromCart, clearAllCart } from "../redux/cartSlice";
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+
+import { increaseQty, decreaseQty, removeFromCart, clearAllCart } from '../redux/cartSlice';
 
 const Cart = () => {
   const cart = useSelector((state) => state.cart);
@@ -13,39 +14,32 @@ const Cart = () => {
             className='btn-close float-end'
             aria-label='Close'
             onClick={() => dispatch(removeFromCart(cartItem.id))}></button>
-          <div className='row justify-content-evenly'>
-            <div className='col-md-4'>
-              <img
-                src={cartItem.image}
-                alt={cartItem.title}
-                height='200px'
-                width='180px'
-              />
+          <div className='d-flex gap-5'>
+            <div className=''>
+              <img src={cartItem.image} alt={cartItem.title} height='100px' width='80px' />
             </div>
-            <div className='col-md-4'>
-              <h4>{cartItem.title}</h4>
-              <p className='lead fw-bold'>Price: ${cartItem.price}</p>
-              <p className='lead fw-medium text-black-50'>
-                Qty: {cartItem.qty} x ${cartItem.price} ={" "}
-                <span className='text-danger'>
-                  ${(cartItem.price * cartItem.qty).toFixed(2)}
-                </span>
-              </p>
-              <button
-                className='btn col-2 btn-outline-dark mx-2'
-                onClick={() => dispatch(increaseQty(cartItem))}>
-                +
-              </button>
-              <button
-                className='btn col-2 btn-outline-dark mx-2'
-                onClick={() => dispatch(decreaseQty(cartItem))}>
-                -
-              </button>
-              <button
-                className='btn col-4 btn-dark mx-2'
-                onClick={() => dispatch(removeFromCart(cartItem.id))}>
-                Remove
-              </button>
+            <div className=''>
+              <h6>{cartItem.title}</h6>
+              <p className='fw-bold'>Price: ${cartItem.price}</p>
+              <div className='fw-medium text-black-50'>
+                Qty: {cartItem.qty} x ${cartItem.price} ={' '}
+                <span className='text-danger'>${(cartItem.price * cartItem.qty).toFixed(2)}</span>
+                <button
+                  className='btn btn-outline-dark mx-2'
+                  onClick={() => dispatch(increaseQty(cartItem))}>
+                  +
+                </button>
+                <button
+                  className='btn btn-outline-dark mx-2'
+                  onClick={() => dispatch(decreaseQty(cartItem))}>
+                  -
+                </button>
+                <button
+                  className='btn btn-dark mx-2'
+                  onClick={() => dispatch(removeFromCart(cartItem.id))}>
+                  Remove
+                </button>
+              </div>
             </div>
           </div>
         </div>
@@ -59,6 +53,11 @@ const Cart = () => {
         <div className='container py-4'>
           <div className='row'>
             <h3>Your Cart is Empty</h3>
+            <h6 className='text-danger'>
+              “Unfortunately, the following item(s) that you ordered are now out-of-stock. Although
+              we try our best to maintain 100% accuracy with inventory, there are rare occasions
+              where we experience an inventory error.”
+            </h6>
           </div>
         </div>
       </div>
@@ -69,9 +68,7 @@ const Cart = () => {
     return (
       <div className='container'>
         <div className='row mb-5 mx-5'>
-          <NavLink
-            to='/checkout'
-            className='btn btn-success col-md-4 col-5  mx-auto'>
+          <NavLink to='/checkout' className='btn btn-success col-md-4 col-5  mx-auto'>
             Proceed To Checkout
           </NavLink>
           <button
