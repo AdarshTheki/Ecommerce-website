e659c8ed2452bb9aa158c7504c7af9189c2c24a9
Adarsh Verma <adarsh878856@gmail.com>
2023-10-19 20:35:29 +0530

    add custom inputs fields components

---
+++ src/utils/Inputs.jsx
@@ -0,0 +1,25 @@
+import React, { forwardRef, useId } from 'react';
+
+const Inputs = forwardRef(function Inputs({ label, type = 'text', className = '', ...props }, ref) {
+  const id = useId();
+
+  return (
+    <div className='form-floating mb-3 shadow card'>
+      <input
+        type={type}
+        id={id}
+        ref={ref}
+        {...props}
+        autoComplete='off'
+        className={`form-control ${className}`}
+      />
+      {label && (
+        <label className='' htmlFor={id}>
+          {label}
+        </label>
+      )}
+    </div>
+  );
+});
+
+export default Inputs;
