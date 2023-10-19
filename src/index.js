1c8f46589539a1dc532e71a749bb785af72beb07
Adarsh Verma <adarsh878856@gmail.com>
2023-10-19 20:33:02 +0530

    import multiple with single file

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import ProductDetail from './pages/ProductDetail';
+import Products from './pages/Products';
+import NotFound from './pages/NotFound';
+import SignUp from './pages/SignUp';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+
+export {
+  About,
+  Cart,
+  Checkout,
+  Contact,
+  Home,
+  ProductDetail,
+  Products,
+  NotFound,
+  SignUp,
+  Footer,
+  Header,
+};
