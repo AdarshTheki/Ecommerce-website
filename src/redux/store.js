4959d24878d9adeca1f91d52129c7d7508ba00bf
Adarsh Verma <adarsh878856@gmail.com>
2023-10-19 20:37:06 +0530

    add store with products, cart, user, and filter used

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
-import productReducer from "./productSlice";
-import cartReducer from "./cartSlice";
-import userReducer from "./userSlice";
-import thunk from "redux-thunk";
+import { configureStore } from '@reduxjs/toolkit';
+import productReducer from './productSlice';
+import cartReducer from './cartSlice';
+import userReducer from './userSlice';
+import filterSlice from './filterSlice';
+import thunk from 'redux-thunk';
 
 export const store = configureStore({
   reducer: {
     products: productReducer,
     cart: cartReducer,
     user: userReducer,
+    filter: filterSlice,
   },
   middleware: [thunk],
 });
